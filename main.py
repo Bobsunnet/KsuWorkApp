0ed57bf039e085454d5c81324d56312d0c4bc09e
bob <bobsunnet@gmail.com>
2023-01-17 18:31:09 +0200

    added text_widget for cell editing and viewing

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ import re
 import refDB
 
 
+class TextWidget(QtWidgets.QTextEdit):
+    def __init__(self):
+        super().__init__()
+        self.linked_cell = None
+
+    def link_cell(self, cell: tuple):
+        self.linked_cell = cell
+
+
 class TableWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(TableWindow, self).__init__()
         self.setup_ui()
         self.table_view_setup()
-        self.buttons_setup()
+        self.widgets_setup()
         self.layout_setup()
-
 
     def table_view_setup(self):
         self.model = refDB.QSqlTableModel(db=refDB.db)
@@ -28,21 +36,28 @@ class TableWindow(QtWidgets.QMainWindow):
 
         self.table_view = QtWidgets.QTableView()
         self.table_view.setModel(self.model)
-        self.table_view.resizeColumnsToContents()
         self.table_view.resizeRowsToContents()
 
+        # адаптивность ширини столбцов
+        header = self.table_view.horizontalHeader()
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(2, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(3, QtWidgets.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(4, QtWidgets.QHeaderView.Stretch)
+
         # self.table_view.selectionModel().selectionChanged.connect(self.cell_changed)
-        self.table_view.selectionModel().currentChanged.connect(self.current_cell_changed)
+        self.table_view.selectionModel().currentChanged.connect(self.cell_highlighted)
 
     def layout_setup(self):
         buttons_layout = QtWidgets.QHBoxLayout()
         buttons_layout_widget = QtWidgets.QWidget()
 
-        main_Vlayout = QtWidgets.QVBoxLayout()
-        main_Vlayout.addWidget(buttons_layout_widget)
-        main_Vlayout.addWidget(self.table_view)
+        text_layout = QtWidgets.QHBoxLayout()
+        text_layout_widget = QtWidgets.QWidget()
+        text_layout_widget.setMaximumHeight(90)
 
-        buttons_layout.addWidget(self.lbl_finder)
+        buttons_layout.addWidget(self.label_finder)
         buttons_layout.addWidget(self.le_finder)
         buttons_layout.addWidget(self.btn_finder)
         buttons_layout.addWidget(self.btn_new_row)
@@ -50,33 +65,106 @@ class TableWindow(QtWidgets.QMainWindow):
         buttons_layout.addWidget(self.btn_show_all)
         buttons_layout.addWidget(self.btn_delete_row)
 
+        text_layout.addWidget(self.label_cell_coords)
+        text_layout.addWidget(self.text_widget)
+
         buttons_layout_widget.setLayout(buttons_layout)
+        text_layout_widget.setLayout(text_layout)
+
+        main_Vlayout = QtWidgets.QVBoxLayout()
+        main_Vlayout.addWidget(buttons_layout_widget)
+        main_Vlayout.addWidget(text_layout_widget)
+        main_Vlayout.addWidget(self.table_view)
 
         main_widget = QtWidgets.QWidget()
         main_widget.setLayout(main_Vlayout)
 
         self.setCentralWidget(main_widget)
 
+    def widgets_setup(self):
+        # _______________________________________BUTTONS_LAYER__________________________________
+        self.label_finder = QtWidgets.QLabel()
+        self.label_finder.setMaximumWidth(70)
+        self.label_finder.setText('Пошук по ID')
 
-    def current_cell_changed(self, current, previous):
-        cell_value = self.model.data(self.model.index(previous.row(), previous.column()))
-        current_row = previous.row()
-        contact_ids = [self.model.data(self.model.index(row, 1)) for row in range(self.model.rowCount())
-                       if row != current_row]
-        if cell_value in contact_ids:
+        self.le_finder = QtWidgets.QLineEdit()
+        self.le_finder.setMaximumWidth(500)
+
+        self.btn_finder = QtWidgets.QPushButton()
+        # self.btn_finder.setMaximumWidth(100)
+        self.btn_finder.setText('Знайти')
+        self.btn_finder.clicked.connect(self.btn_finder_clicked)
+
+        self.btn_new_row = QtWidgets.QPushButton()
+        # self.btn_new_row.setMaximumWidth(100)
+        self.btn_new_row.setText('+')
+        self.btn_new_row.clicked.connect(self.btn_new_row_clicked)
+
+        self.btn_save_all = QtWidgets.QPushButton()
+        # self.btn_save_all.setMaximumWidth(100)
+        self.btn_save_all.setText('Зберегти')
+        self.btn_save_all.clicked.connect(self.btn_save_clicked)
+
+        self.btn_show_all = QtWidgets.QPushButton()
+        # self.btn_show_all.setMaximumWidth(100)
+        self.btn_show_all.setText('Показати все')
+        self.btn_show_all.clicked.connect(self.btn_show_all_clicked)
+
+        self.btn_delete_row = QtWidgets.QPushButton()
+        # self.btn_delete_row.setMaximumWidth(100)
+        self.btn_delete_row.setText('Видалити')
+        self.btn_delete_row.clicked.connect(self.btn_delete_row_clicked)
+
+        # __________________________________TEXT_LAYER_________________________
+        self.text_widget = TextWidget()
+        self.text_widget.setMaximumHeight(70)
+        self.text_widget.cursorPositionChanged.connect(self.change_cell_text)
+        self.text_widget.setAcceptRichText(False)
+
+        self.label_cell_coords = QtWidgets.QLabel()
+        self.label_cell_coords.setText('Row: \nCol: ')
+        self.label_cell_coords.setMaximumWidth(60)
+        self.label_cell_coords.setMinimumWidth(40)
+
+    # __________________________________ LOGIC _______________________________________
+
+    def cell_highlighted(self, current, previous):
+        current_row, current_col = current.row(), current.column()
+        self.text_widget.link_cell((current_row, current_col))
+        #меняем текст в виджете
+        self.change_text_widget(current_row, current_col)
+        self.label_cell_change(current_row, current_col)
+
+        self.chek_lsukr(previous.row(), previous.column())
+
+    def chek_lsukr(self, row, col):
+        cell_value = self.model.data(self.model.index(row, col))
+        current_row = row
+        # создаем список всех LSUKR и сравниваем есть ли уже такой как в измененной ячейке
+        lsukr_list = [self.model.data(self.model.index(row, 1)) for row in range(self.model.rowCount())
+                      if row != current_row]
+        if cell_value in lsukr_list:
             self.btn_show_all_clicked()
 
+    def change_text_widget(self, row, col):
+        text = self.model.data(self.model.index(row, col))
+        self.text_widget.setText(str(text))
 
-    # def cell_changed(self, selected, deselected):
-    #     for cell in deselected.indexes():
-    #         print(f'cell row: {cell.row()}, column: {cell.column()} changed')
+    def label_cell_change(self, row, col):
+        self.label_cell_coords.setText(f'Row: {row} \nCol: {col} ')
+
+    def change_cell_text(self):
+        text = self.text_widget.toPlainText()
+        if self.text_widget.linked_cell:
+            row, col = self.text_widget.linked_cell
+            self.model.setData(self.model.index(row, col), text)
 
     def filter_model(self, s):
         self.model.setFilter(s)
 
     def btn_finder_clicked(self):
         text = self.le_finder.text()
-        text_safe = re.sub(r'\D+', '', text) #allows only digits to go through filter
+        text_safe = re.sub(r'\D+', '', text)  # allows only digits to go through filter
         filtered_string = 'contact_id LIKE "%{}%"'.format(text_safe)
         self.filter_model(filtered_string)
 
@@ -124,44 +212,9 @@ class TableWindow(QtWidgets.QMainWindow):
     def btn_save_clicked(self):
         pass
 
-    def buttons_setup(self):
-        self.lbl_finder = QtWidgets.QLabel()
-        self.lbl_finder.setMaximumWidth(70)
-        self.lbl_finder.setText('Пошук по ID')
-
-        self.le_finder = QtWidgets.QLineEdit()
-        self.le_finder.setMaximumWidth(500)
-
-        self.btn_finder = QtWidgets.QPushButton()
-        # self.btn_finder.setMaximumWidth(100)
-        self.btn_finder.setText('Знайти')
-        self.btn_finder.clicked.connect(self.btn_finder_clicked)
-
-        self.btn_new_row = QtWidgets.QPushButton()
-        # self.btn_new_row.setMaximumWidth(100)
-        self.btn_new_row.setText('+')
-        self.btn_new_row.clicked.connect(self.btn_new_row_clicked)
-
-        self.btn_save_all = QtWidgets.QPushButton()
-        # self.btn_save_all.setMaximumWidth(100)
-        self.btn_save_all.setText('Зберегти')
-        self.btn_save_all.clicked.connect(self.btn_save_clicked)
-
-        self.btn_show_all = QtWidgets.QPushButton()
-        # self.btn_show_all.setMaximumWidth(100)
-        self.btn_show_all.setText('Показати все')
-        self.btn_show_all.clicked.connect(self.btn_show_all_clicked)
-
-        self.btn_delete_row = QtWidgets.QPushButton()
-        # self.btn_delete_row.setMaximumWidth(100)
-        self.btn_delete_row.setText('Видалити')
-        self.btn_delete_row.clicked.connect(self.btn_delete_row_clicked)
-
-
     def setup_ui(self):
         self.setWindowTitle('Table Main Window')
         self.setMinimumSize(800, 400)
-
 
 
 app = QtWidgets.QApplication(sys.argv)
@@ -170,5 +223,3 @@ mainWindow = TableWindow()
 mainWindow.show()
 
 sys.exit(app.exec_())
-
-
